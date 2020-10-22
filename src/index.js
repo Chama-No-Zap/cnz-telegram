e810057ab32aa7f28b4438d3fb4e1bc650d8f1ab
Henrique Rezende <henriquerezendec@gmail.com>
2020-10-21 22:14:36 -0300

    Tests

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ const Telegraf = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply("Hello world"));
+bot.start((ctx) => ctx.reply(`Olá, poderia me confirmar se seu nome é ${ctx.from.first_name} ${ctx.from.last_name}?`));
 
-bot.hears('ola', (ctx) => ctx.reply('tchau'))
+bot.hears('ola', (ctx) => ctx.reply(`Olá ${ctx.from.first_name}?`))
+
+bot.hears('Sim', (ctx) => ctx.reply(`Prazer, ${ctx.from.first_name}, preciso que você insira o CNPJ do seu negócio :)`));
 
 bot.on('text', (ctx) => {
   console.log(ctx.message.text, ctx.from, ctx.chat);
-  return ctx.reply('Recebi um texto');
+  return ctx.reply('Agora digite o nome fantasia do seu negócio');
 });
 
 
